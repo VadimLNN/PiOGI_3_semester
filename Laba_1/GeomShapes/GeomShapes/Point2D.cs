da58c7cccef38bdd150344b55666a2104ca12390
VadimLNN <svobodolubsev@inbox.ru>
2023-06-19 14:51:24 +0700

    создал основу для 1 лабы, написал первый класс не полностью, подготовил задание в комментах

---
+++ Laba_1/GeomShapes/GeomShapes/Point2D.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeomShapes
+{
+    internal class Point2D
+    {
+        private double x;
+        private double y;
+
+        public Point2D(double acquired_x, double acquired_y)
+        {
+            x = acquired_x;
+            y = acquired_y;
+        }
+
+        public double getX()
+        {
+            return x;
+        }
+
+        public double getY()
+        {
+            return y;
+        }
+    
+        public void shiftX(double shift)
+        {
+
+        }
+
+        public void shiftY(double shift)
+        {
+
+        }
+
+        public double GetDistance()
+        {
+            double res = 0;
+
+            return res;
+        }
+    }
+}
