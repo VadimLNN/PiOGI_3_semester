7d1a43728f2c35125821c297d241062e79d362ea
Vadim LNN <svobodolubsev@inbox.ru>
2023-09-03 20:27:24 +0700

    добавил описание

--- Laba_1/GeomShapes/GeomShapes/Point2D.cs
+++ Laba_1/GeomShapes/GeomShapes/Point2D.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+    1) Класс «двумерная точка». Класс должен содержать поля для хранения координат по осям X и       Y
+    Y и методы, реализующие следующие операции:
+         сдвиг точки по осям X и Y на заданное расстояние;                                         (Y)
+         вычисление расстояния между двумя точками;                                                (Y)
+*/
+
 namespace GeomShapes
 {
     public class Point2D
@@ -11,16 +18,8 @@ namespace GeomShapes
         private double x;
         private double y;
 
-        public Point2D()
-        {
-            x = 0;
-            y = 0;
-        }
-        public Point2D(double X, double Y)
-        {
-            x = X;
-            y = Y;
-        }
+        public Point2D() { x = 0; y = 0; }
+        public Point2D(double X, double Y) { x = X; y = Y; }
 
         public Point2D(Point2D point)
         {
@@ -29,15 +28,12 @@ namespace GeomShapes
         }
 
         public double getX() { return x; }
-
         public double getY() { return y; }
 
         public void setX(double X) { x = X; }
-
         public void setY(double Y) { y = Y; }
 
         public void shiftX(double shift) { x += shift; }
-
         public void shiftY(double shift) { y += shift; }
 
         public double getDistance(Point2D next)
