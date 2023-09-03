8e2841321b6d8f12233639c012f5e9e1b9fc6d3f
Vadim LNN <svobodolubsev@inbox.ru>
2023-09-03 20:36:05 +0700

    переаисал часть функций сделал перестановку

--- Laba_1/GeomShapes/GeomShapes/MainWindow.xaml.cs
+++ Laba_1/GeomShapes/GeomShapes/MainWindow.xaml.cs
@@ -23,44 +23,30 @@ using System.Windows.Shapes;
     
     2) Класс «треугольник». Класс должен содержать поля для хранения вершин треугольника и           Y
     методы, реализующие следующие операции:
-         вычисление площади и периметра;                                                           (NO)
+         вычисление площади и периметра;                                                           (Y)
          сдвиг треугольника по осям X и Y на заданное расстояние;                                  (Y)    
     
-    3) Класс «прямоугольник». Класс должен содержать поля для хранения вершин прямоугольника        NO
+    3) Класс «прямоугольник». Класс должен содержать поля для хранения вершин прямоугольника         Y
     и методы, реализующие следующие операции:   
-         вычисление площади и периметра;                                                           (NO)    
-         сдвиг прямоугольника по осям X и Y на заданное расстояние;                                (NO)
+         вычисление площади и периметра;                                                           (Y)    
+         сдвиг прямоугольника по осям X и Y на заданное расстояние;                                (Y)
     
-    4) Класс для генерации геометрических фигур. Класс должен содержать статические методы          NO
+    4) Класс для генерации геометрических фигур. Класс должен содержать статические методы           N
     создания геометрических фигур:
-         создание произвольной («рандомной») точки;                                                (NO)
-         создание произвольного треугольника;                                                      (NO)
-         создание произвольного прямоугольника;                                                    (NO)
-         создание прямоугольника заданного размера.                                                (NO)
+         создание произвольной («рандомной») точки;                                                (N)
+         создание произвольного треугольника;                                                      (N)
+         создание произвольного прямоугольника;                                                    (N)
+         создание прямоугольника заданного размера.                                                (N)
 */
 namespace GeomShapes
 {
     public partial class MainWindow : Window
     {
-        Point2D p = new Point2D();
-        
+
         Random rng = new Random();
-        
+        Point2D p = new Point2D();
         Triangle tr = new Triangle();
-        void drawPoint(Point2D point){
-            Ellipse myEllipse = new Ellipse();
-            myEllipse.Stroke = System.Windows.Media.Brushes.Black;
-            myEllipse.Fill = System.Windows.Media.Brushes.White;
-            myEllipse.HorizontalAlignment = HorizontalAlignment.Left;
-            myEllipse.VerticalAlignment = VerticalAlignment.Center;
-            
-            myEllipse.Width = 7;
-            myEllipse.Height = 7;
-
-            myEllipse.RenderTransform = new TranslateTransform(point.getX(), point.getY());
-
-            scene.Children.Add(myEllipse);
-        }
+        Rectangle rec = new Rectangle();
         
         void drawLine(Point2D start_p, Point2D end_p)  {
             Line line = new Line();
@@ -85,37 +71,37 @@ namespace GeomShapes
 
         private void DrawPoint_Click(object sender, RoutedEventArgs e)
         {
-            p.setX(double.Parse(x_coordinate.Text));
-            p.setY(double.Parse(y_coordinate.Text));
-
+            if (field_1.Text == "" && field_2.Text == "")
+            {
+                //ShapeGeneration.genRandPoint2D();
+                p.setX(rng.NextDouble() * 560);
+                p.setY(rng.NextDouble() * 388);
+            }
+            else
+            { 
+                p.setX(double.Parse(field_1.Text));
+                p.setY(double.Parse(field_2.Text));
+            }
             scene.Children.Clear();
 
             drawPoint(p);
         }
-
-        private void vert_bar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        void drawPoint(Point2D point)
         {
-            double value = e.NewValue - e.OldValue;
+            Ellipse myEllipse = new Ellipse();
+            myEllipse.Stroke = System.Windows.Media.Brushes.Black;
+            myEllipse.Fill = System.Windows.Media.Brushes.White;
+            myEllipse.HorizontalAlignment = HorizontalAlignment.Left;
+            myEllipse.VerticalAlignment = VerticalAlignment.Center;
             
-            //if(e.NewValue != tr.getA().getY())
-                p.shiftY(value);
+            myEllipse.Width = 7;
+            myEllipse.Height = 7;
 
-            scene.Children.Clear();
+            myEllipse.RenderTransform = new TranslateTransform(point.getX(), point.getY());
 
-            drawPoint(p);
+            scene.Children.Add(myEllipse);
         }
-
-        private void hor_bar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            double value = e.NewValue - e.OldValue;
-
-            //if (e.NewValue != tr.getA().getX())
-                p.shiftX(value);
-
-            scene.Children.Clear();
-
-            drawPoint(p);
-        }
+        
 
         private void DrawTriangle_Click(object sender, RoutedEventArgs e)
         {
@@ -127,12 +113,95 @@ namespace GeomShapes
 
             scene.Children.Clear();
 
+            drawTriangle(tr);
+
+            pr_res.Content = tr.getPerimeter();
+            sq_res.Content = tr.getArea();
+        }
+        void drawTriangle(Triangle tr)
+        {
             drawLine(tr.getA(), tr.getB());
             drawLine(tr.getB(), tr.getC());
             drawLine(tr.getC(), tr.getA());
-
-            pr_res.Content = tr.perimeter();
-            sq_res.Content = tr.square();
         }
+        
+
+        private void DrawRectangle_Click(object sender, RoutedEventArgs e)
+        {
+            Point2D a = new Point2D(rng.NextDouble() * 560, rng.NextDouble() * 388);
+            Point2D b = new Point2D(rng.NextDouble() * 560, rng.NextDouble() * 388);
+            Point2D c = new Point2D(rng.NextDouble() * 560, rng.NextDouble() * 388);
+            Point2D d = new Point2D(rng.NextDouble() * 560, rng.NextDouble() * 388);
+
+            rec = new Rectangle(a, b, c, d);
+
+            scene.Children.Clear();
+
+            drawRectangle(rec);
+
+            pr_res.Content = rec.getPerimeter();
+            sq_res.Content = rec.getArea();
+        }
+        void drawRectangle(Rectangle rec)
+        {
+            drawLine(rec.getA(), rec.getB());
+            drawLine(rec.getB(), rec.getC());
+            drawLine(rec.getC(), rec.getD());
+            drawLine(rec.getD(), rec.getA());
+        }
+    
+
+        private void vert_bar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            double value = e.NewValue - e.OldValue;
+
+            if (scene.Children.Capacity == 1)
+            {
+                scene.Children.Clear();
+                p.shiftY(value);
+                drawPoint(p);
+            }
+            else if (scene.Children.Capacity == 3)
+            {
+                scene.Children.Clear();
+                tr.shiftY(value);
+                drawTriangle(tr);
+            }
+            else if (scene.Children.Capacity == 4)
+            {
+                scene.Children.Clear();
+                rec.shiftY(value);
+                drawRectangle(rec);
+            }
+        
+            
+        }
+        private void hor_bar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            double value = e.NewValue - e.OldValue;
+
+            if (scene.Children.Capacity == 1)
+            {
+                scene.Children.Clear();
+                p.shiftX(value);
+                drawPoint(p);
+            }
+            else if (scene.Children.Capacity == 3)
+            {
+                scene.Children.Clear();
+                tr.shiftX(value);
+                drawTriangle(tr);
+            }
+            else if (scene.Children.Capacity == 4)
+            {
+                scene.Children.Clear();
+                rec.shiftX(value);
+                drawRectangle(rec);
+            }
+        }
+
+        
+            
+        
     }
 }
