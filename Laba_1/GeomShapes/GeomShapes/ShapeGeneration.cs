a4dc966e6481ed5160baf43c27b7e8e9dea3ab8d
Vadim LNN <svobodolubsev@inbox.ru>
2023-09-03 20:34:59 +0700

    создал класс и добавил описание

---
+++ Laba_1/GeomShapes/GeomShapes/ShapeGeneration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    4) Класс для генерации геометрических фигур. Класс должен содержать статические методы           N
+    создания геометрических фигур:
+         создание произвольной («рандомной») точки;                                                (N)
+         создание произвольного треугольника;                                                      (N)
+         создание произвольного прямоугольника;                                                    (N)
+         создание прямоугольника заданного размера.                                                (N) 
+*/
+namespace GeomShapes
+{
+    internal class ShapeGeneration
+    {
+        
+    }
+}
