da7ac64960ff14ec7e1b6e7aee611a94bed514d9
Vadim LNN <svobodolubsev@inbox.ru>
2023-09-07 17:57:39 +0700

    написал 4 функции, генерация ранд точки, треугольника, прямоугольника, заданного прямоугольника

--- Laba_1/GeomShapes/GeomShapes/ShapeGeneration.cs
+++ Laba_1/GeomShapes/GeomShapes/ShapeGeneration.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 /*
     4) Класс для генерации геометрических фигур. Класс должен содержать статические методы           N
     создания геометрических фигур:
-         создание произвольной («рандомной») точки;                                                (N)
-         создание произвольного треугольника;                                                      (N)
+         создание произвольной («рандомной») точки;                                                (Y)
+         создание произвольного треугольника;                                                      (Y)
          создание произвольного прямоугольника;                                                    (N)
          создание прямоугольника заданного размера.                                                (N) 
 */
@@ -16,6 +16,45 @@ namespace GeomShapes
 {
     internal class ShapeGeneration
     {
+        public static Point2D genRandPoint2D()
+        {
+            Random rng = new Random();
+            return new Point2D(rng.NextDouble() * 568, rng.NextDouble() * 343);
+        }
         
+        public static Triangle genRandTriangle()
+        {
+            Random rng = new Random();
+
+            Point2D a = new Point2D(rng.NextDouble() * 568, rng.NextDouble() * 343);
+            Point2D b = new Point2D(rng.NextDouble() * 568, rng.NextDouble() * 343);
+            Point2D c = new Point2D(rng.NextDouble() * 568, rng.NextDouble() * 343);
+
+            return new Triangle(a, b, c);
+        }
+        
+
+        public static Rectangle genRandRectangle()
+        {
+            Random rng = new Random();
+
+            Point2D a = new Point2D(rng.NextDouble() * 100, rng.NextDouble() * 100);
+            Point2D b = new Point2D(rng.NextDouble() * (568 - a.getX()), a.getY());
+            Point2D c = new Point2D(b.getX(), rng.NextDouble() * (343 - a.getY()));
+            Point2D d = new Point2D(a.getX() ,c.getY());
+
+            return new Rectangle(a, b, c, d);
+        }
+        public static Rectangle genRectangle(double width, double height)
+        {
+            Random rng = new Random();
+
+            Point2D a = new Point2D( rng.NextDouble() * 100, rng.NextDouble() * 100 );
+            Point2D b = new Point2D( a.getX() + width, a.getY()) ;
+            Point2D c = new Point2D( b.getX(), a.getY() + height );
+            Point2D d = new Point2D( a.getX(), c.getY());
+
+            return new Rectangle(a, b, c, d);
+        }
     }
 }
