65cb34582100de621b19df54abd242adab359efa
Vadim LNN <svobodolubsev@inbox.ru>
2023-09-18 15:33:01 +0700

    теперь можно ставить маркер машины на карту

--- Laba_2/GeoInformApp/GeoInformApp/MainWindow.xaml.cs
+++ Laba_2/GeoInformApp/GeoInformApp/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using GMap.NET;
 using GMap.NET.MapProviders;
 using GMap.NET.WindowsPresentation;
 using System.Device.Location;
+using System.Windows.Media.Media3D;
 /*
 Разработать геоинформационное приложение на базе библиотеки GMap.NET, а также:
     1. Реализовать отображение пользовательских объектов в виде маркеров на карте: местоположение, 
@@ -39,14 +40,56 @@ using System.Device.Location;
 
     5. Для каждого из объектов реализовать метод для определения расстояния до произвольной точки. 
         С помощью этого метода реализовать поиск ближайших объектов.
- */
+*/
 namespace GeoInformApp
 {
     public partial class MainWindow : Window
     {
+        PointLatLng point = new PointLatLng(55.012823, 82.950359);
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MapLoaded(object sender, RoutedEventArgs e)
+        {
+            // настройка доступа к данным
+            GMaps.Instance.Mode = AccessMode.ServerAndCache;
+
+            // установка провайдера карт
+            Map.MapProvider = GoogleMapProvider.Instance;
+            // GoogleMapProvider     +
+            // OpenCycleMapProvider  +
+
+            // установка зума карты
+            Map.MinZoom = 2;
+            Map.MaxZoom = 17;
+            Map.Zoom = 15;
+            // установка фокуса карты
+            Map.Position = new PointLatLng(55.012823, 82.950359);
+
+            // настройка взаимодействия с картой
+            Map.MouseWheelZoomType = MouseWheelZoomType.MousePositionAndCenter;
+            Map.CanDragMap = true;
+            Map.DragButton = MouseButton.Left;
+        }
+
+        private void Map_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            PointLatLng point = Map.FromLocalToLatLng((int)e.GetPosition(Map).X, (int)e.GetPosition(Map).Y);
+
+            GMapMarker marker = new GMapMarker(point)
+            {
+                Shape = new Image
+                {
+                    Width = 32, // ширина маркера
+                    Height = 32, // высота маркера
+                    ToolTip = "Car", // всплывающая подсказка
+                    Source = new BitmapImage(new Uri("pack://application:,,,/imgs/car.png")) // картинка
+                }
+            };
+
+            Map.Markers.Add(marker);
         }
     }
 }
