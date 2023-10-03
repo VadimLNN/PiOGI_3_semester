c683ba8d60c976bbe19f3a847085e497e3bcf7f9
Vadim LNN <svobodolubsev@inbox.ru>
2023-10-03 21:18:48 +0700

    прописал класс для постороения пути

--- Laba_2/GeoInformApp/GeoInformApp/Route.cs
+++ Laba_2/GeoInformApp/GeoInformApp/Route.cs
@@ -1,12 +1,63 @@
-﻿using System;
+﻿using GMap.NET.WindowsPresentation;
+using GMap.NET;
+using System;
 using System.Collections.Generic;
+using System.Device.Location;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Windows.Media;
 
 namespace GeoInformApp
 {
-    internal class Route
+    internal class Route : MapObject
     {
+        List <PointLatLng> locations;
+        GMapMarker path;
+        public Route(string title, List <PointLatLng> locations) : base(title)
+        {
+             this.locations = locations;
+
+            if (locations.Count < 2)
+                return;
+
+            path = new GMapRoute(locations)
+            {
+                Shape = new System.Windows.Shapes.Path()
+                {
+                    Stroke = Brushes.DarkBlue, // цвет обводки
+                    Fill = Brushes.DarkBlue, // цвет заливки
+                    StrokeThickness = 4 // толщина обводки
+                }
+            };
+
+        }
+        public override double getDistance(PointLatLng point)
+        {
+            double min = 0;
+            GeoCoordinate c2 = new GeoCoordinate(point.Lat, point.Lng);
+
+            for (int i = 0; i < locations.Count; i++)
+            {
+                GeoCoordinate c1 = new GeoCoordinate(locations[i].Lat, locations[i].Lng);
+
+                if (min < c2.GetDistanceTo(c1))
+                    min = c2.GetDistanceTo(c1);
+            }
+
+            return min; 
+        }
+
+        public override PointLatLng getFocus()
+        {
+            return locations[0];
+        }
+
+        public override GMapMarker getMarker()
+        {
+            return path;
+        }
     }
 }
