ea842e793f4743fe8263737413800d756673abf9
Vadim LNN <svobodolubsev@inbox.ru>
2023-10-03 21:17:25 +0700

    прописал класс для выделения зоны

--- Laba_2/GeoInformApp/GeoInformApp/Area.cs
+++ Laba_2/GeoInformApp/GeoInformApp/Area.cs
@@ -1,12 +1,60 @@
-﻿using System;
+﻿using GMap.NET.WindowsPresentation;
+using GMap.NET;
+using System;
 using System.Collections.Generic;
+using System.Device.Location;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace GeoInformApp
 {
-    internal class Area
+    public class Area : MapObject
     {
+        List<PointLatLng> locations;
+        GMapMarker area;
+        public Area(string title, List<PointLatLng> locations) : base(title)
+        {
+            this.locations = locations;
+
+            if (locations.Count < 3)
+                return;
+
+            area = new GMapPolygon(locations)
+            {
+                Shape = new System.Windows.Shapes.Path
+                {
+                    Stroke = Brushes.Black, // стиль обводки
+                    Fill = Brushes.LightGoldenrodYellow, // стиль заливки
+                    Opacity = 0.5 // прозрачность
+                }
+            };
+
+        }
+        public override double getDistance(PointLatLng point)
+        {
+            double min = 0;
+            GeoCoordinate c2 = new GeoCoordinate(point.Lat, point.Lng);
+            for (int i = 0; i < locations.Count; i++)
+            {
+                GeoCoordinate c1 = new GeoCoordinate(locations[i].Lat, locations[i].Lng);
+
+                if (min < c2.GetDistanceTo(c1))
+                    min = c2.GetDistanceTo(c1);
+            }
+
+            return min;
+        }
+
+        public override PointLatLng getFocus()
+        {
+            return locations[0];
+        }
+
+        public override GMapMarker getMarker()
+        {
+            return area;
+        }
     }
 }
