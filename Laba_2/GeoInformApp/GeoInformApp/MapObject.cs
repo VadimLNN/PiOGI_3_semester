b1e7e48c911c6f774786c4cc712662d5b5e9acb5
Vadim LNN <svobodolubsev@inbox.ru>
2023-10-03 19:57:12 +0700

    написал интерфейс главного класса

--- Laba_2/GeoInformApp/GeoInformApp/MapObject.cs
+++ Laba_2/GeoInformApp/GeoInformApp/MapObject.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GMap.NET;
+using GMap.NET.WindowsPresentation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,23 @@ using System.Threading.Tasks;
 
 namespace GeoInformApp
 {
-    internal class MapObject
+    public abstract class MapObject
     {
+        string title;
+        DateTime creationDate;
+
+        public MapObject(string title, DateTime creationDate)
+        {
+            this.title = title;
+        }
+
+        string getTitle() { return title; }
+        DateTime getCreationDate() { return creationDate; }
+
+        public abstract double getDistance(PointLatLng point);
+
+        public abstract PointLatLng getFocus();
+
+        public abstract GMapMarker GetMarker();
     }
 }
