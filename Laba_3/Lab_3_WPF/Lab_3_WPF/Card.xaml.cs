db3ca351388919eded5d63fd64e4325923e599d3
Vadim LNN <svobodolubsev@inbox.ru>
2023-11-05 22:24:43 +0700

    создал пользовательский интерфейс: карточка

---
+++ Laba_3/Lab_3_WPF/Lab_3_WPF/Card.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_3_WPF
+{
+    public partial class Card : UserControl
+    {
+        public static readonly DependencyProperty CardNameProperty = DependencyProperty.Register(
+                                                                 "Name", // имя параметра в разметке
+                                                                 typeof(string), // тип данных параметра
+                                                                 typeof(Card), // тип данных элемента управления
+                                                                 // метаданные - значение параметра по умолчанию и обработчик изменения параметра
+                                                                 new PropertyMetadata(string.Empty, NameChanged));
+
+        public static readonly DependencyProperty CardPicProperty = DependencyProperty.Register(
+                                                                 "CardPic", // имя параметра в разметке
+                                                                 typeof(ImageSource), // тип данных параметра
+                                                                 typeof(Card), // тип данных элемента управления
+                                                                 // метаданные - значение параметра по умолчанию и обработчик изменения параметра
+                                                                 new PropertyMetadata(null, PicChanged));
+
+        public string Cardname
+        {
+            get { return (string)GetValue(CardNameProperty); }
+            set { SetValue(CardNameProperty, value); }
+        }
+
+        public ImageSource Pic
+        {
+            get { return (ImageSource)GetValue(CardPicProperty); }
+            set { SetValue(CardPicProperty, value); }
+        }
+
+
+        public static void NameChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var loginForm = d as Card;
+            loginForm.Name.Content = loginForm.Cardname;
+        }
+        public static void PicChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var loginForm = d as Card;
+            loginForm.CardPic.Source = loginForm.Pic;
+        }
+
+        public Card()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Laba_3/Lab_3_WPF/Lab_3_WPF/MainWindow.xaml.cs
+++ Laba_3/Lab_3_WPF/Lab_3_WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +16,41 @@ using System.Windows.Shapes;
 
 namespace Lab_3_WPF
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void bt_1_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            string fname = ofd.FileName;
+
+            card_1.Name.Content = tb_1.Text;
+            card_1.Pic = new BitmapImage(new Uri(fname));
+        }
+
+        private void bt_2_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            string fname = ofd.FileName;
+
+            card_2.Name.Content = tb_2.Text;
+            card_2.Pic = new BitmapImage(new Uri(fname));
+        }
+
+        private void bt_3_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            string fname = ofd.FileName;
+
+            card_3.Name.Content = tb_3.Text;
+            card_3.Pic = new BitmapImage(new Uri(fname));
         }
     }
 }
