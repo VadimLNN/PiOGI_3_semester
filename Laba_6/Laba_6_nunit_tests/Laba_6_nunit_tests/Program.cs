8dde1df81885311280ce87557bb00fba09fe9874
VadimLNN <svobodolubsev@inbox.ru>
2023-11-06 23:13:16 +0700

    прописал функции и тесты по заданию

--- Laba_6/Laba_6_nunit_tests/Laba_6_nunit_tests/Program.cs
+++ Laba_6/Laba_6_nunit_tests/Laba_6_nunit_tests/Program.cs
@@ -3,35 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*
+    1. Реализовать набор функций:
+         функция конвертации дюймов в сантиметры;
+         функция проверки числа на чётность;
+         функция выбора наибольшего значения из массива целых чисел;
+         функция вычисления остатка от деления.
+    В случае возникновения ошибки, функции должны возвращать код ошибки.
 
+    2. Написать набор тестовых сценариев для реализованных тестовых функций. Тестовые
+        сценарии должны охватывать все ветви выполнения функций.
+ */
 namespace Laba_6_nunit_tests
 {
     internal class Calculator
     {
-        public int add(int x, int y) {
-                return x + y;    
-        }
-        public int sub(int x, int y)
-        {
-            return x - y;
+        // ############## 1 ##############
+        public double inches_to_cm(int a)
+        { 
+            return a * 2.54; 
         }
 
-        public int mod(int a, int b)
-        {
-            if (b <= 0) throw new ArgumentException("Делитель должен быть >= 0");
-            return a % b;
-        }
-
+        // ############## 2 ##############
         public bool isEven(int n)
         {
             return n % 2 == 0;
         }
 
+        // ############## 3 ##############
+        public int max_in_arr(int[] a)
+        {
+            return a.Max(); 
+        }
+
+        // ############## 4 ##############
+        public int remainder_of_division(int a, int b) 
+        {
+            return a % b;
+        }
 
         static void Main(string[] args)
         {
-            
-
 
         }
     }
