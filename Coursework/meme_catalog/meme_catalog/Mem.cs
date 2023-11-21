fce798af50fcb32954e66b7ac338276c0f4c4893
VadimLNN <svobodolubsev@inbox.ru>
2023-11-21 17:27:16 +0700

    прописал класс мем

---
+++ Coursework/meme_catalog/meme_catalog/Mem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace meme_catalog
+{
+    internal class Mem
+    {
+        public int UID { get; set; }
+        public string Name { get; set; }
+        public string Img { get; set; }
+        public string Tag { get; set; }
+        public Mem(string name, string img)
+        {
+            Name = name;
+            Img = img;
+        }
+        public Mem(string name, string img, string tag)
+        {
+            Name = name;
+            Img = img;
+            Tag = tag;
+        }
+
+    }
+}
